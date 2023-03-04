9ba3256c18b12e38dfcd79e8efced0e5a900cf28
AbdulNasirQader <nasakanasir1@gmail.com>
2023-03-04 15:54:01 -0500

    SeleniumReviewBasics

---
+++ src/SeleniumReview01/LaunchApplication.java
@@ -0,0 +1,14 @@
+package SeleniumReview01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchApplication {
+    public static void main(String[] args) {
+
+        //1. Open the browser
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Abdul Nasir Qader\\IdeaProjects\\SeleniumReviewBasics\\Driver\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+    }
+}
