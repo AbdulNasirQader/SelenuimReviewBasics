313f9114ce7ec42dbe851f6d3d78eae6be94001f
AbdulNasirQader <nasakanasir1@gmail.com>
2023-03-14 10:08:15 -0400

    Selenium Review Class

--- src/SeleniumReview01/LaunchApplication2.java
+++ src/SeleniumReview01/LaunchApplication2.java
@@ -2,12 +2,17 @@ package SeleniumReview01;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LaunchApplication2 {
     public static void main(String[] args) {
 
-        WebDriver driver=new ChromeDriver();
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
       //  driver.get("https://www.google.com/");
         driver.navigate().to("https://google.com/");
+        String pageTitle= driver.getTitle();
+        System.out.println(pageTitle);
     }
 }
